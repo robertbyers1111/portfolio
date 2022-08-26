ac4f0f9dff79c1a1241748012b73249ddba19a60
robertbyers1111 <robertbyers1111@gmail.com>
2022-08-26 00:45:53 -0400

    Improved documentation

--- tides/tidesapp/datetime_utils.py
+++ tides/tidesapp/datetime_utils.py
@@ -7,10 +7,26 @@ from datetime import datetime, date
 
 def day2datetime(queried_day):
     """
-    Returns a python datetime object representing the inputted day, auto filling year and month.
+    Accepts a day-of-month integer. Returns a datetime object equivalent.
 
-    The year and month are assumed to be the current year and month, unless day < today, in
-    which case the month is assumed to be the subsequent month.
+    This is not a generic method. This method supports *ONLY* the
+    requirements of the tidesapp application.
+
+    The motivation for this method is that the tideschart.com weekly
+    tides tables display only a day-of-month integer. This DOM-rendered
+    table displays these day-of-month values relative to the current 
+    day. Therefore it is possible to determine the actual date referenced
+    by each of these day-of-month numbers.
+
+    For example, if the user visits the table on 7/4/2022 and is presented
+    with tides data for the 4th, 5th, 6th, 7th, 8th, 9th and 10th of the
+    month, we know these are all relative to 7/4/2022.
+
+    This method uses this knowlege to convert a day-of-month integer from
+    the weekly tides table to a Python datetime value.
+
+    Appropriate handling of month overflow and year overflow conditions
+    are fully supported.
 
     Motivation: This method exists because the tideschart.com weekly tide table provides *only*
     the day-of-month number for the tides covering the next seven days. Since the first row
